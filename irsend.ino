aa76767ba895e88e115ee0625f15160bf3d9bb76
sanjunt98 <sanjunt98@gmail.com>
2018-04-11 13:22:24 +0530

    Add files via upload

---
+++ irsend.ino
@@ -0,0 +1,28 @@
+#include <IRLib.h>
+#include <IRLibMatch.h>
+#include <IRLibRData.h>
+#include <IRLibTimer.h>
+
+//#include <IRLibAll.h>
+     
+IRsend mySender;
+     
+void setup() {
+ Serial.begin(9600);
+  }
+     
+ void loop() {
+   if (Serial.read() != -1)
+   {
+        
+      mySender.send(NEC,0x667FC8A1, 32);
+      }
+    delay(100);
+    }
+/*    
+6B45A4E6
+667FC8A1
+667FC8A1
+667FC8A1
+667FC8A1
+*/
